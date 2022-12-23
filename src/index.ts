cef1b90e3188140414da633815a437757c7c7a1a
DavidIacovone <iacovone.david@gmail.com>
2022-12-23 23:05:52 +0100

    Added string sorting

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { CharactersCollection } from "./charactersCollection";
 import { NumbersCollection } from "./numbersCollection";
 
 const numbersCollection = new NumbersCollection([4, 6, 2, 0, -4]);
-const charactersCollection = new CharactersCollection('bsksbvkbsfhbhbbubwhbciybcwbvi');
+const charactersCollection = new CharactersCollection('XXXaaaABB');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data);
